0a6674d9d1ac1a91188a6c95c8d3cb89458b4349
Manank Patni <manank321@gmail.com>
2018-10-02 18:00:28 +0530

    Added travis and some features

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, session, redirect, url_for
 from app import app
 from app.firebase_connect import Get_Polls,Prev_Polls, Get_Members
+from datetime import datetime as dt
+import pytz
+
+now = dt.now(pytz.timezone('Asia/Kolkata'))
 
 gp=Get_Polls()
 pp=Prev_Polls()
@@ -12,10 +16,12 @@ def index():
 	if 'username' in session:
 		user=session['username']
 		group=session['group']
+		user_type=session['user_type']
 		polls=gp.get_poll_by_group(session['group'])
 		prev_polls=pp.get_previous_polls(user)
 		members=gm.get_group_members(user, group)
-		return render_template('index.html',all_polls=polls, prev_polls=prev_polls, members=members)
+		return render_template('index.html',all_polls=polls, prev_polls=prev_polls, 
+			members=members,user_type=session['user_type'],current_date=str(now))
 	return redirect(url_for('signin'))
 
 @app.route('/forms')
